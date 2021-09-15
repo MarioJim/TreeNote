d0bf99a0af93f8d79ca4610003b0354e0fae2a11
MarioJim <mario.emilio.j@gmail.com>
2021-09-15 11:43:24 -0500

    Start Java project and diagrams

---
+++ src/main/java/com/swdesignteam/TreeNote/TreeNoteApplication.java
@@ -0,0 +1,13 @@
+package com.swdesignteam.TreeNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swdesignteam/TreeNote/TreeNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swdesignteam.TreeNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
